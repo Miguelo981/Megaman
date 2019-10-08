2fb92fc703dd9a0cf5252dd80362919d1d0ec522
alu2018240 <lorito20@hotmail.es>
2019-10-08 20:16:02 +0200

    first code

--- menu.py
+++ menu.py
@@ -1,8 +1,27 @@
 import time
 import pygame
 
-ANCHO = 800
-ALTO = 600
-color_negro_fondo = (0,0,0) #color de fondo
-color_blanco = (255,255,255) #color del texto
-pygame.init()
+def start_window():
+    global pantalla
+    width = 800
+    higth = 600
+    color_negro_fondo = (0, 0, 0)
+    color_blanco = (255, 255, 255)
+    pygame.init()
+
+    pantalla = pygame.display.set_mode((width, higth))
+    pygame.display.set_caption("Megaman")
+
+def start_menu():
+    start_window()
+    #reloj para medir la velocidad
+    reloj = pygame.time.Clock()
+    #ajustamos la repetición de la tecla pulsada
+    pygame.key.set_repeat(30)
+    puntuacion = 0 #agregamos la puntuacion
+    vidas = 3 #agregamos vidas al jugador
+
+def get_pantalla():
+    return pantalla
+
+start_menu()
