04244a7f62ace6f121e9e8d8b3b316afd58b4582
alu2018240 <lorito20@hotmail.es>
2019-11-22 16:55:09 +0100

    jump and dash movement update

--- objects.py
+++ objects.py
@@ -1,7 +1,19 @@
 # Sprite classes for platform game
+import glob
+import os
+
 import pygame as pg
 from Settings import *
 vec = pg.math.Vector2
+
+
+def load_images(folder_path):
+    images = []
+    os.chdir(folder_path)
+    for files in sorted(glob.glob('*.png'), key=os.path.getmtime):
+        images.append(folder_path + '/' + files)
+    return images
+
 
 class Player(pg.sprite.Sprite):
     def __init__(self, game):
@@ -9,21 +21,34 @@ class Player(pg.sprite.Sprite):
         self.game = game
         #self.image = pg.Surface((30, 40))
         self.image = pg.Surface((14, 34))
-        self.image = pg.image.load('images/MM_WS.png')
+        self.spawn_sprites = load_images(self.path + '\images\MM_spawn')
+        self.spawn()
+        self.image = pg.image.load(os.getcwd()+'\images\MM_WS.png') #images/MM_WS.png
         #self.image.fill(YELLOW)
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH / 2, HEIGHT / 2)
         self.pos = vec(WIDTH / 2, HEIGHT / 2)
         self.vel = vec(0, 0)
         self.acc = vec(0, 0)
+        self.right = True
+        self.moving = False
         self.dashing = False
         self.timer = 0
         #self.last = pygame.time.get_ticks()
         self.clock = self.game.clock
         self.cooldown = 100
+        self.animation_cooldown = 15
+        self.animation_counter = 0
         self.counter = 0
+        self.count = 0
         self.time = clock.tick()
         self.collide = False
+        self.path = os.getcwd()
+        self.move_right_sprites = load_images(self.path+'\images\MM_move_r')
+        self.move_left_sprites = load_images(self.path+'\images\MM_move_l')
+
+    def spawn(self):
+        pass #TODO SPAWN ANIMATION
 
     def jump(self):
         # jump only if standing on a platform
@@ -36,18 +61,45 @@ class Player(pg.sprite.Sprite):
     def set_clock(self):
         self.time = self.clock.tick()
         self.counter += self.time
+        self.animation_counter += self.time
         #print(self.counter)
 
     def update(self):
         self.acc = vec(0, PLAYER_GRAV)
         keys = pg.key.get_pressed()
+        if self.vel.x >= 0 and self.vel.x < 1.5 or self.vel.x > -1.5 and self.vel.x <= 0:
+            self.moving = False
+            self.count = 0
+
+        if self.count == 8:
+            self.count = 0
+
+        if self.moving:
+            if self.right:
+                self.image = pg.image.load(self.move_right_sprites[self.count])
+            else:
+                self.image = pg.image.load(self.move_left_sprites[self.count])
+            if self.animation_counter > self.animation_cooldown:
+                self.count += 1
+                self.animation_counter = 0
+        else:
+            if self.right:
+                self.image = pg.image.load(self.path+'\images\MM_WS.png')
+            else:
+                self.image = pg.image.load(self.path + '\images\MM_WS_l.png')
+
         if keys[pg.K_LEFT] and not self.collide:
             self.acc.x = -PLAYER_ACC
+            self.right = False
+            self.moving = True
         if keys[pg.K_RIGHT] and not self.collide:
             self.acc.x = PLAYER_ACC
+            self.right = True
+            self.moving = True
         #TODO CONTROL DEL DASH
         if keys[pg.K_a] and keys[pg.K_LEFT]:
             if self.counter > self.cooldown:
+                self.moving = True
                 for i in range(1,5):
                     self.vel.x -= 0.75
                     hits = pg.sprite.spritecollide(self, self.game.platforms, False)
@@ -59,6 +111,7 @@ class Player(pg.sprite.Sprite):
                 #self.vel.x = 0
 
         if keys[pg.K_a] and keys[pg.K_RIGHT]:
+            self.moving = True
             if self.counter > self.cooldown:
                 for i in range(1, 5):
                     self.vel.x += 1.5
