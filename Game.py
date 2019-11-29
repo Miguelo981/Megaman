1666ca294ffe23ba169e3140caee13fd6aa96f32
alu2018240 <lorito20@hotmail.es>
2019-11-29 16:59:54 +0100

    jump and dash movement update

--- Game.py
+++ Game.py
@@ -9,9 +9,33 @@ class Game:
         pg.init()
         pg.mixer.init()
         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        self.display = pg.Surface((300, 200))
         pg.display.set_caption(TITLE)
         self.clock = pg.time.Clock()
         self.running = True
+
+    def charge_map(self):
+        '''true_scroll[0] += (self.player.rect.x - true_scroll[0] - 152) / 20
+        true_scroll[1] += (self.player.rect.y - true_scroll[1] - 106) / 20
+        scroll = true_scroll.copy()
+        scroll[0] = int(scroll[0])
+        scroll[1] = int(scroll[1])'''
+        tile_rects = []
+        y = 0
+        for layer in game_map:
+            x = 0
+            for tile in layer:
+                if tile == '1':
+                    self.display.blit(dirt_img, (x * 16, y * 16))
+                if tile == '2':
+                    self.display.blit(grass_img, (x * 16, y * 16))
+                if tile != '0':
+                    tile_rects.append(pygame.Rect(x * 16, y * 16, 16, 16))
+                #self.all_sprites.add(t)
+                #self.platforms.add(t)
+                x += 1
+            y += 1
+        self.player.rect = self.move(tile_rects)
 
     def new(self):
         # start a new game
@@ -19,10 +43,10 @@ class Game:
         self.platforms = pg.sprite.Group()
         self.player = Player(self)
         self.all_sprites.add(self.player)
-        for plat in PLATFORM_LIST:
+        '''for plat in PLATFORM_LIST:
             p = Platform(*plat)
             self.all_sprites.add(p)
-            self.platforms.add(p)
+            self.platforms.add(p)'''
         self.run()
 
     def run(self):
@@ -32,15 +56,18 @@ class Game:
             self.player.set_clock()
             self.clock.tick(FPS)
             self.events()
+            self.charge_map()
             self.update()
             self.draw()
 
     def update(self):
+        #self.display.blit(pygame.transform.flip(self.player))
         self.player.spawn()
         # Game Loop - Update
         self.all_sprites.update()
+
         # check if player hits a platform - only if falling
-        if self.player.vel.y > 0:
+        if self.player.vel.y > 0 and False:
             hits = pg.sprite.spritecollide(self.player, self.platforms, False)
             #hits = pg.sprite.collide_rect(self.player, self.platforms)
             if hits:
@@ -120,6 +147,37 @@ class Game:
             self.player.pos = vec(WIDTH / 2, HEIGHT / 2)
             #self.player.kill()
 
+    def collision_test(self, tiles):
+        hit_list = []
+        for tile in tiles:
+            if self.player.rect.colliderect(tile):
+                hit_list.append(tile)
+            '''if self.player.rect.colliderect(tile):
+                hit_list.append(tile)'''
+        return hit_list
+
+    def move(self, tiles):
+        collision_types = {'top': False, 'bottom': False, 'right': False, 'left': False}
+        #self.player.rect.x += self.player.vel.x
+        hit_list = self.collision_test(tiles)
+        for tile in hit_list:
+            if self.player.vel.x > 0:
+                self.player.rect.right = tile.left
+                collision_types['right'] = True
+            elif self.player.vel.x < 0: #movement[0] < 0
+                self.player.rect.left = tile.right
+                collision_types['left'] = True
+        self.player.rect.y += self.player.vel.y
+        hit_list = self.collision_test(tiles)
+        for tile in hit_list:
+            if self.player.vel.y > 0:
+                self.player.rect.bottom = tile.top
+                collision_types['bottom'] = True
+            elif self.player.vel.y < 0:
+                self.player.rect.top = tile.bottom
+                collision_types['top'] = True
+        return collision_types
+
     def events(self):
         # Game Loop - events
         for event in pg.event.get():
@@ -135,8 +193,9 @@ class Game:
     def draw(self):
         # Game Loop - draw
         self.screen.fill(BLACK)
-        self.all_sprites.draw(self.screen)
+        ###self.all_sprites.draw(self.screen)
         # *after* drawing everything, flip the display
+        #screen.blit(pygame.transform.scale(self.display, (WIDTH, HEIGHT)), (0, 0))
         pg.display.flip()
 
     def show_start_screen(self):
